0166864a23242a613de8ab9bcdb696abea69ca82
elena-gireva <94042128+elena-gireva@users.noreply.github.com>
2022-08-26 08:30:59 +0300

    my commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import image from './shopping.jpg';
+import imageTwo from './man.jpg'
+import { GrocerList } from './GroceryList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <img src = { image } alt = "grocery" width = "200px"/>
+      </div>
+      <div className='container'>
+        <h2>Grocery List</h2>
+      </div>
+      <GrocerList/>
+      <div className='container'>
+        <img src = { imageTwo } alt = "man" width="200px"/>
+      </div>
     </div>
   );
 }
